384483ef5fa46f9d9617bb8cbb5d95d22defdc44
Metodi Gitsev <m.gitsev@gmail.com>
2022-11-03 11:34:03 +0100

    Boam se

--- src/components/ProductFilter/Sorter.jsx
+++ src/components/ProductFilter/Sorter.jsx
@@ -12,14 +12,14 @@ export default function Sorter() {
   return (
     <div className="mx-2 text-center">
       <label
-        for="categories"
-        class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+        htmlFor="categories"
+        className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
       >
         Sort by
       </label>
       <select
         id="categories"
-        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         value={productSorter}
         onChange={updateSorter}
       >
--- src/components/ProductFilter/index.jsx
+++ src/components/ProductFilter/index.jsx
@@ -4,7 +4,7 @@ import Sorter from "./Sorter";
 
 export default function ProductFilter() {
   return (
-    <div className="w-full p-5 flex flex-row justify-center flex-nowrap bg-gray-300">
+    <div className="w-full p-5 flex sm:flex-col flex-row justify-center flex-nowrap bg-gray-300">
       <CategoryFilter />
       <Sorter />
       <Orderer />

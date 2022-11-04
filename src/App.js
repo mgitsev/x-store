e53d7d4fe917f0d5b563d7ecb90a6e20fb2bd354
Metodi Gitsev <m.gitsev@gmail.com>
2022-11-04 18:25:29 +0100

    Error page added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Navigation from "./components/Navigation";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import { useEffect } from "react";
@@ -7,14 +7,16 @@ import { getProducts } from "./apis/products.mjs";
 import { useSetRecoilState } from "recoil";
 import { productListState } from "./state/products.mjs";
 import Product from "./pages/Product";
+import Error from "./pages/Error";
 
 export default function App() {
   const setProducts = useSetRecoilState(productListState);
+  const navi = useNavigate();
 
   useEffect(() => {
     getProducts()
       .then(({ products }) => setProducts(products))
-      .catch((e) => console.log(e));
+      .catch((e) => navi("/404"));
   });
   return (
     <>
@@ -24,6 +26,7 @@ export default function App() {
           <Route path="/" element={<Home categories={["categories"]} />} />
           <Route path="/products" element={<Products />} />
           <Route path="/products/:id" element={<Product />} />
+          <Route path="/404" element={<Error />} />
         </Routes>
       </main>
     </>
--- src/apis/products.mjs
+++ src/apis/products.mjs
@@ -1,15 +1,14 @@
 import axios from "axios";
 
 const baseURL = "https://dummyjson.com/products";
-export async function getProducts(cb, limit = 100, skip = 0) {
+export async function getProducts(limit = 100, skip = 0) {
   const url = `${baseURL}?limit=${limit}&skip=${skip}`;
   try {
     const { data } = await axios.get(url);
     return data;
   } catch (err) {
     console.log(err);
-    // hendlaj greshka so callback u useEffect
-    //cb(err)
+    return err;
   }
 }
 
